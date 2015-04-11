e13b12b91a1f0f74dad8be8a00ebd41951b02402
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-04-12 08:58:52 +0900

    Add function to speak YES

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 module.exports = function (text) {
-	if (text.length !== 0){
+	if (text === 'みんなが思ってる私の可愛さ') {
+		console.log(text + 'YES!');
+		console.log('止まらないんだからぁ');
+	} else if (text.length !== 0) {
 		console.log(text + ' あったかいんだからぁ');
 	} else {
 		tomaranaindakaraa();
