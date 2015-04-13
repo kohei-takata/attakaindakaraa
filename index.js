9943bf390aa032ada7d1dc4eff19632edf0e06d5
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-04-14 08:48:27 +0900

    Refactoring for adding tests

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 module.exports = function (text) {
+	console.log(main(text));
+}
+
+function main(text) {
 	if (text === 'みんなが思ってる私の可愛さ') {
-		console.log(text + 'YES!');
-		console.log('止まらないんだからぁ');
+		return text + ' YES! 止まらないんだからぁ';
 	} else if (text.length !== 0) {
-		console.log(text + ' あったかいんだからぁ');
+		return text + ' あったかいんだからぁ';
 	} else {
 		tomaranaindakaraa();
 	}
